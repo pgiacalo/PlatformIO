f70c436440c0a079f404d0061152039719a2e00b
Philip Giacalone <philip_giacalone@yahoo.com>
2023-01-07 04:37:06 -0800

    add esp32 function generator code

--- ESP32_dacWrite_sine_wave/src/main.cpp
+++ ESP32_dacWrite_sine_wave/src/main.cpp
@@ -1,184 +1,57 @@
-/* DAC Cosine Generator Example
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
+#include <Arduino.h>
 
-#include "soc/rtc_io_reg.h"
-#include "soc/rtc_cntl_reg.h"
-#include "soc/sens_reg.h"
-#include "soc/rtc.h"
+#define UNDEFINED_WAVE  0
+#define SINE_WAVE       1
+#define SQUARE_WAVE     2
+#define TRIANGLE_WAVE   3
 
+const float deg_to_rad = PI/180;
 
-#include "driver/dac.h"
+//ESP32 has two 8-bit DAC (digital to analog converter) channels, connected to GPIO25 (Channel 1) and GPIO26 (Channel 2)
+//out_voltage = Vref * digi_val / 255
+//Vref is internal (ESP32 does NOT support an external voltage reference)
+int OUTPUT_PIN = 25;
 
-/* Declare global sine waveform parameters
- * so they may be then accessed and changed from debugger
- * over an JTAG interface
- */
-int clk_8m_div = 0;      // RTC 8M clock divider (division is by clk_8m_div+1, i.e. 0 means 8MHz frequency)
-int frequency_step = 8;  // Frequency step for CW generator
-int scale = 1;           // 50% of the full scale
-int offset;              // leave it default / 0 = no any offset
-int invert = 2;          // invert MSB to get sine waveform
+int WAVE_TYPE = SINE_WAVE;
 
+float angles[360] = {};
 
-/*
- * Enable cosine waveform generator on a DAC channel
- */
-void dac_cosine_enable(dac_channel_t channel)
-{
-    // Enable tone generator common to both channels
-    SET_PERI_REG_MASK(SENS_SAR_DAC_CTRL1_REG, SENS_SW_TONE_EN);
-    switch(channel) {
-        case DAC_CHANNEL_1:
-            // Enable / connect tone tone generator on / to this channel
-            SET_PERI_REG_MASK(SENS_SAR_DAC_CTRL2_REG, SENS_DAC_CW_EN1_M);
-            // Invert MSB, otherwise part of waveform will have inverted
-            SET_PERI_REG_BITS(SENS_SAR_DAC_CTRL2_REG, SENS_DAC_INV1, 2, SENS_DAC_INV1_S);
-            break;
-        case DAC_CHANNEL_2:
-            SET_PERI_REG_MASK(SENS_SAR_DAC_CTRL2_REG, SENS_DAC_CW_EN2_M);
-            SET_PERI_REG_BITS(SENS_SAR_DAC_CTRL2_REG, SENS_DAC_INV2, 2, SENS_DAC_INV2_S);
-            break;
-        default :
-           printf("Channel %d\n", channel);
+void setup() {
+  Serial.begin(115200);
+}
+
+boolean firstLoop = true;
+
+void loop() {
+  //store the angles in an array 
+  if (firstLoop == true) {
+    for (int deg = 0; deg < 360; deg++){
+      angles[deg] = deg * deg_to_rad;
+      firstLoop = false;
     }
-}
+  }
 
+  for (int deg = 0; deg < 360; deg++){
+    float val = angles[deg];
+//    printf("Angle in Radians = %f\n", val);
 
-/*
- * Set frequency of internal CW generator common to both DAC channels
- *
- * clk_8m_div: 0b000 - 0b111
- * frequency_step: range 0x0001 - 0xFFFF
- *
- */
-void dac_frequency_set(int clk_8m_div, int frequency_step)
-{
-    REG_SET_FIELD(RTC_CNTL_CLK_CONF_REG, RTC_CNTL_CK8M_DIV_SEL, clk_8m_div);
-    SET_PERI_REG_BITS(SENS_SAR_DAC_CTRL1_REG, SENS_SW_FSTEP, frequency_step, SENS_SW_FSTEP_S);
-}
-
-
-/*
- * Scale output of a DAC channel using two bit pattern:
- *
- * - 00: no scale
- * - 01: scale to 1/2
- * - 10: scale to 1/4
- * - 11: scale to 1/8
- *
- */
-void dac_scale_set(dac_channel_t channel, int scale)
-{
-    switch(channel) {
-        case DAC_CHANNEL_1:
-            SET_PERI_REG_BITS(SENS_SAR_DAC_CTRL2_REG, SENS_DAC_SCALE1, scale, SENS_DAC_SCALE1_S);
-            break;
-        case DAC_CHANNEL_2:
-            SET_PERI_REG_BITS(SENS_SAR_DAC_CTRL2_REG, SENS_DAC_SCALE2, scale, SENS_DAC_SCALE2_S);
-            break;
-        default :
-           printf("Channel %d\n", channel);
+    if (WAVE_TYPE == SINE_WAVE){
+      dacWrite(25, int(100 + 64 * (sin(angles[deg]))));
+    } else if (WAVE_TYPE == SQUARE_WAVE){
+      dacWrite(OUTPUT_PIN, int(128 + 80 * (sin(angles[deg])+sin(3*angles[deg])/3+sin(5*angles[deg])/5+sin(7*angles[deg])/7+sin(9*angles[deg])/9+sin(11*angles[deg])/11)));
+    } else if (WAVE_TYPE == TRIANGLE_WAVE){
+      dacWrite(OUTPUT_PIN, int(128 + 80 * (sin(angles[deg])+1/pow(3,2)*sin(3*angles[deg])+1/pow(5,2)*sin(5*angles[deg])+1/pow(7,2)*sin(7*angles[deg])+1/pow(9,2)*sin(9*angles[deg])))); 
+    } else {
+      //default to this...
+      dacWrite(DAC1, 128);//255= 3.3V 128=1.65V
+      delay(100);
     }
+  }
 }
 
+// Square wave   = amplitude . sin(x) + sin(3.x) / 3 +  sin (5.x) / 5 + sin (7.x) / 7  + sin (9.x) / 9  + sin (11.x) / 11  Odd harmonics
+// Triangle wave = amplitude . sin(x) - 1/3^2.sin(3.x) +  1/5^2.sin(5.x) - 1/7^2.sin (7.x) + 1/9^2.sin(9.x) - 1/11^2.sin (11.x) Odd harmonics
+// dacWrite(25, int(128 + 80 * (sin(angles[deg])))); // GPIO Pin mode (OUTPUT) is set by the dacWrite function
+// dacWrite(25, int(128 + 80 * (sin(angles[deg])+sin(3*angles[deg])/3+sin(5*angles[deg])/5+sin(7*angles[deg])/7+sin(9*angles[deg])/9+sin(11*angles[deg])/11))); // Square
+// dacWrite(25, int(128 + 80 * (sin(angles[deg])+1/pow(3,2)*sin(3*angles[deg])+1/pow(5,2)*sin(5*angles[deg])+1/pow(7,2)*sin(7*angles[deg])+1/pow(9,2)*sin(9*angles[deg])))); // Triangle
 
-/*
- * Offset output of a DAC channel
- *
- * Range 0x00 - 0xFF
- *
- */
-void dac_offset_set(dac_channel_t channel, int offset)
-{
-    switch(channel) {
-        case DAC_CHANNEL_1:
-            SET_PERI_REG_BITS(SENS_SAR_DAC_CTRL2_REG, SENS_DAC_DC1, offset, SENS_DAC_DC1_S);
-            break;
-        case DAC_CHANNEL_2:
-            SET_PERI_REG_BITS(SENS_SAR_DAC_CTRL2_REG, SENS_DAC_DC2, offset, SENS_DAC_DC2_S);
-            break;
-        default :
-           printf("Channel %d\n", channel);
-    }
-}
-
-
-/*
- * Invert output pattern of a DAC channel
- *
- * - 00: does not invert any bits,
- * - 01: inverts all bits,
- * - 10: inverts MSB,
- * - 11: inverts all bits except for MSB
- *
- */
-void dac_invert_set(dac_channel_t channel, int invert)
-{
-    switch(channel) {
-        case DAC_CHANNEL_1:
-            SET_PERI_REG_BITS(SENS_SAR_DAC_CTRL2_REG, SENS_DAC_INV1, invert, SENS_DAC_INV1_S);
-            break;
-        case DAC_CHANNEL_2:
-            SET_PERI_REG_BITS(SENS_SAR_DAC_CTRL2_REG, SENS_DAC_INV2, invert, SENS_DAC_INV2_S);
-            break;
-        default :
-           printf("Channel %d\n", channel);
-    }
-}
-
-/*
- * Main task that let you test CW parameters in action
- *
-*/
-void dactask(void* arg)
-{
-    while(1){
-
-        // frequency setting is common to both channels
-        dac_frequency_set(clk_8m_div, frequency_step);
-
-        /* Tune parameters of channel 2 only
-         * to see and compare changes against channel 1
-         */
-        dac_scale_set(DAC_CHANNEL_2, scale);
-        dac_offset_set(DAC_CHANNEL_2, offset);
-        dac_invert_set(DAC_CHANNEL_2, invert);
-
-        float frequency = RTC_FAST_CLK_FREQ_APPROX / (1 + clk_8m_div) * (float) frequency_step / 65536;
-        printf("clk_8m_div: %d, frequency step: %d, frequency: %.0f Hz\n", clk_8m_div, frequency_step, frequency);
-        printf("DAC2 scale: %d, offset %d, invert: %d\n", scale, offset, invert);
-        vTaskDelay(2000/portTICK_PERIOD_MS);
-    }
-}
-
-
-/*
- * Generate a sine waveform on both DAC channels:
- *
- * DAC_CHANNEL_1 - GPIO25
- * DAC_CHANNEL_2 - GPIO26
- *
- * Connect scope to both GPIO25 and GPIO26
- * to observe the waveform changes
- * in response to the parameter change
-*/
-void app_main()
-{
-    dac_cosine_enable(DAC_CHANNEL_1);
-    dac_cosine_enable(DAC_CHANNEL_2);
-
-    dac_output_enable(DAC_CHANNEL_1);
-    dac_output_enable(DAC_CHANNEL_2);
-
-    xTaskCreate(dactask, "dactask", 1024*3, NULL, 10, NULL);
-}
